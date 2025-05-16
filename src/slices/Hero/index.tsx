28ed29b574dcb46c50da09f25c242d5f0d056c0c
MG10327 <54483612+MG10327@users.noreply.github.com>
2025-05-16 15:24:55 -0400

    add static hero images

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import {Content} from '@prismicio/client'
+import { PrismicNextImage, PrismicNextLink } from '@prismicio/next'
+import {PrismicRichText, SliceComponentProps} from '@prismicio/react'
 
-type Props = {}
+// Props for the hero go here
+export type HeroProps = SliceComponentProps<Content.HeroSlice>
 
-const index = (props: Props) => {
-  return (
-    <div>index</div>
-  )
+
+// component for the hero slices
+const Hero = ({slice}: HeroProps): JSX.Element => {
+    return (
+        <section>
+            data-slice-type={slice.slice_type}
+            data-slice-type={slice.variation}
+
+            <PrismicRichText field={slice.primary.heading} />
+            <PrismicRichText field={slice.primary.heading} />
+            <PrismicRichText field={slice.primary.heading} />
+            {slice.primary.button_text}
+            <PrismicNextLink field={slice.primary.button_link}>
+                Link
+            </PrismicNextLink>
+            <PrismicNextImage field={slice.primary.cans_image}/>
+            <PrismicRichText field={slice.primary.second_heading} />
+            <PrismicRichText field={slice.primary.second_body} />
+        </section>
+
+    )
 }
 
-export default index
+export default Hero
